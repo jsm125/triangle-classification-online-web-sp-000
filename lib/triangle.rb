a82fe597919e44c9017c6dfe4335b9df5f124160
Matt Im <matthew.seiim@gmail.com>
2020-05-14 20:25:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,14 +16,15 @@ class Triangle
         (@length_a + @length_b <= @length_c) || (@length_a + @length_c <= @length_b) || (@length_b + @length_c <= @length_a)
         raise TriangleError
       else
-         if (@length_a == @length_b) || (@length_b == @length_c) || (@length_a == @length_c)
-          :isosceles
+
       elsif
           (@length_a == @length_b) && (@length_b == @length_c)
           :equilateral
       elsif
           (@length_a != @length_b) && (@length_b != @length_c) && (@length_a != @length_c)
           :scalene
+           if (@length_a == @length_b) || (@length_b == @length_c) || (@length_a == @length_c)
+  :isosceles
       end
       end   
     end
