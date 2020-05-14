163ba6076edc3279fe1c496eaa0beac4b0f13844
Matt Im <matthew.seiim@gmail.com>
2020-05-14 19:37:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,10 +12,10 @@ class Triangle
   class TriangleError < StandardError
   
     def kind()
-      if (@length_a <= 0) || (@length_b <= 0), || (@length_c <= 0)
+      if (@length_a <= 0) || (@length_b <= 0) || (@length_c <= 0)
         raise TriangleError
       elsif 
-        (@length_a + @length_b <= @length_c), || (@length_a + @length_c <= @length_b) || (@length_b + @length_c <= @length_a)
+        (@length_a + @length_b <= @length_c) || (@length_a + @length_c <= @length_b) || (@length_b + @length_c <= @length_a)
         raise TriangleError
       else
         if
