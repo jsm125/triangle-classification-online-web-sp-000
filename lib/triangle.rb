a2c590b0279301944f14872e5256c45d77338b70
Matt Im <matthew.seiim@gmail.com>
2020-05-14 18:30:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+lib/triangle.rb 
+
 class Triangle
-  # write code here
+  attr_accessor :length_a, :length_b, :length_c
+  
+  def initialize(length_a, length_b, length_c)
+    @length_a = length_a
+    @length_b = length_b
+    @length_c = length_c
+  end
+  
+  def kind 
+    if length_a != length_b || length_c
+      scalene
+    elsif
+    if length_a == length_b && length_b != length_c
+      isosceles
+    else
+      equilateral
+    end
+      
+  
+  
 end
